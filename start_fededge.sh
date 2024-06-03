7ce69461ee626c2a8fef98bbd31ca03632c5e505
trungdonggg <trungdonggg@gmail.com>
2024-06-03 11:33:08 +0700

    fix error sh fed edge and server

--- start_fededge.sh
+++ start_fededge.sh
@@ -3,4 +3,4 @@ echo "Starting Orchestrator of EADRAN"
 
 export PYTHONPATH="${PYTHONPATH}:$PWD"
 
-python3 fed_edge/orchestrator/start_edge_service.py --conf=fed_edge/conf/config.json &
+python3 fed_edge/orchestration/start_edge_service.py --conf=fed_edge/conf/config.json &
--- start_fedserver.sh
+++ start_fedserver.sh
@@ -3,4 +3,4 @@ echo "Starting Fed Server of EADRAN"
 
 export PYTHONPATH="${PYTHONPATH}:$PWD"
 
-python3 fed_server/orchestrator/start_fed_server_service.py --conf=fed_server/conf/config.json &
+python3 fed_server/orchestration/start_fed_server_service.py --conf=fed_server/conf/config.json &
