1a4f1f4f68122305145e7c310aa40b46ebf7d642
dungcao <ctdung258@gmail.com>
2024-06-03 12:57:11 +0700

    Put all start commands in .sh

--- start_fededge.sh
+++ start_fededge.sh
@@ -3,4 +3,4 @@ echo "Starting Edge Orchestration Service of EADRAN"
 
 export PYTHONPATH="${PYTHONPATH}:$PWD"
 
-python3 fed_edge/orchestration/start_edge_service.py --conf=fed_edge/conf/config.json
+python3 fed_edge/orchestration/start_edge_service.py --conf=fed_edge/conf/config.json &
