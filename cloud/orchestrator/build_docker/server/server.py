1b8752092b1f08d2b25878f8c5cb0f16b43508b8
dungcao <ctdung258@gmail.com>
2024-10-21 11:08:15 +0700

    experimenting water leak...

--- cloud/orchestrator/build_docker/server/server.py
+++ cloud/orchestrator/build_docker/server/server.py
@@ -39,8 +39,8 @@ if __name__ == "__main__":
 
     # Define strategy
     strategy = fl.server.strategy.FedAvg(
-        min_fit_clients=params.clients,
-        min_evaluate_clients=params.clients,
+        min_fit_clients=int(params.clients),
+        min_evaluate_clients=int(params.clients),
         on_fit_config_fn=fit_config,
         on_evaluate_config_fn=evaluate_config,
         evaluate_metrics_aggregation_fn=weighted_average)
