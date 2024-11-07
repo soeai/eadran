735a84606ca9263637ecd4c480b09971a5761939
dungcao <ctdung258@gmail.com>
2024-11-07 11:35:48 +0700

    experimenting water leak...

--- tester/orchestrator/tester.py
+++ tester/orchestrator/tester.py
@@ -24,35 +24,59 @@
 
 import pandas as pd
 import numpy as np
-# from cloud.orchestrator.build_docker.qod.evaluate import completeness, class_parity, label_purity
+from sklearn.preprocessing import StandardScaler
 
-# df = pd.read_csv("/Users/dungcao/Desktop/water_leak/water_leak_10_20241027132623.csv")
-df = pd.read_csv("../../apps/water_leak/data_preprocessing/preprocessData/scenario_2/split_5.csv")
+from cloud.orchestrator.build_docker.qod.evaluate import *
+
+df = pd.read_csv("/Users/Storage/Research/EdgeML/eadran_water_leak/split_1_mice.csv")
+# df = pd.read_csv("../../apps/water_leak/data_preprocessing/preprocessData/scenario_2/split_1.csv")
 
 # null_count = df.isnull().sum().sum()
 # total_count = np.prod(df.shape)
 #
 # print(np.around(1 - null_count / total_count, 4))
-print(df['target'].value_counts())
+# print(df['target'].value_counts())
 y = df['target']
-X = df.drop(['target'], axis=1)
+X = df.drop(['target','time'], axis=1)
+# print(X.max())
+#
+# df = pd.DataFrame(X)
+# null_count = df.isnull().sum().sum()
+# total_count = np.prod(df.shape)
+#
+# print(np.around(1 - null_count / total_count, 4))
+#
+# # print(completeness(X))
+# values, counts = np.unique(y, return_counts=True)
+# max_count = max(counts)
+# # print(max_count)
+# counts = counts/max_count
+# # print(counts)
+# mean_count = counts.mean()
+# # print(mean_count)
+# counts = counts - mean_count
+# # print(counts)
+# print(np.around(1 - np.absolute(counts).sum()/len(counts),4))
 
-df = pd.DataFrame(X)
-null_count = df.isnull().sum().sum()
-total_count = np.prod(df.shape)
+# X = df[np.isfinite(X).all(1)]
 
-print(np.around(1 - null_count / total_count, 4))
-
-# print(completeness(X))
-values, counts = np.unique(y, return_counts=True)
-max_count = max(counts)
-# print(max_count)
-counts = counts/max_count
-# print(counts)
-mean_count = counts.mean()
-# print(mean_count)
-counts = counts - mean_count
-# print(counts)
-print(np.around(1 - np.absolute(counts).sum()/len(counts),4))
+_com = completeness(X)
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
+# np.nan_to_num(X, copy=False)
+# X = np.asarray(X,dtype=np.float32)
+# print(type(X))
+# print(X.isnull().sum())
+# print("class_overlap: ", class_overlap(X, y))
+# print("class_parity: ", class_parity(y))
+# print("label_purity: ", label_purity(X, y))
+qod_metrics = {"class_overlap": class_overlap(X, y),
+               "class_parity": class_parity(y),
+               # "label_purity": label_purity(X, y),
+               "feature_correlation": feature_correlation(X),
+               "feature_relevance": feature_relevance(X, y, 0.9),
+               "completeness": _com}
+#
+print(qod_metrics)
 
 
