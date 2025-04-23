8eacf22b7f3f5b4dd8aa5ab3f548066627e29580
dungcao <ctdung258@gmail.com>
2025-04-23 18:00:27 +0700

    fixed cost service by using built-in functions...

--- eadran_cost_service/src/main/scala/org/eadran/cost/Functions.scala
+++ eadran_cost_service/src/main/scala/org/eadran/cost/Functions.scala
@@ -11,22 +11,22 @@ object Functions {
   val functionMap: Map[String, Seq[Any] => Double] = Map(
     "qom_cost_by_train_performance" -> { args =>
       val qom = args(0).asInstanceOf[QualityOfModel]
-      val cost = args(1).asInstanceOf[String]
-      qom_cost_by_train_performance(qom, cost)
+      val base_cost_metric = args(1).asInstanceOf[String]
+      qom_cost_by_train_performance(qom, base_cost_metric)
     },
     "resource_cost_by_cpu_mem" -> { args =>
       val resource = args(0).asInstanceOf[ResourceMonitor]
       val training_time = args(1).asInstanceOf[Double]
-      val cost = args(2).asInstanceOf[String]
-      resource_cost_by_cpu_memory(resource, training_time, cost)
+      val base_cost_metric = args(2).asInstanceOf[String]
+      resource_cost_by_cpu_memory(resource, training_time, base_cost_metric)
     }
   )
 
-  def qom_cost_by_train_performance(measurement: QualityOfModel, unit_costs: String) ={
+  def qom_cost_by_train_performance(measurement: QualityOfModel, base_cost_metric: String) ={
     var value = 0.0
     try {
-      var ucosts = parse(unit_costs).extract[BaseCostQoM]
-      value = math.max(0.0, (ucosts.performance * (measurement.post_train_performance
+      var basecosts = parse(base_cost_metric).extract[BaseCostQoM]
+      value = math.max(0.0, (basecosts.performance * (measurement.post_train_performance
           - measurement.pre_train_performance) * measurement.pre_train_performance))
     }catch {
       case e: Exception => println("JSON object of unit cost is not correct!")
@@ -34,12 +34,13 @@ object Functions {
     value
   }
 
-  def resource_cost_by_cpu_memory(measurement: ResourceMonitor, training_time: Double, unit_costs: String) ={
+  def resource_cost_by_cpu_memory(measurement: ResourceMonitor, training_time: Double, base_cost_metric: String) ={
     var value = 0.0
     try {
-      var basecosts = parse(unit_costs).extract[BaseCostResource]
+      var basecosts = parse(base_cost_metric).extract[BaseCostResource]
       value = basecosts.by_minute * training_time *
-        (basecosts.cpu.getOrElse(0.0) * measurement.cpu_percentage + basecosts.memory.getOrElse(0.0) * measurement.memory_usage)
+        (basecosts.cpu.getOrElse(0.0) * measurement.cpu_percentage
+          + basecosts.memory.getOrElse(0.0) * measurement.memory_usage)
     }catch {
       case e: Exception => println("JSON object of unit cost is not correct!")
     }
